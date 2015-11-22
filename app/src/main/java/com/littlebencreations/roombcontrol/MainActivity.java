3166af5ff73184a4a999c28ea3a29cabc65395c4
austin <austin.cawley@gmail.com>
2015-11-22 18:31:46 -0500

    Some structures n such

--- app/src/main/java/com/littlebencreations/roombcontrol/MainActivity.java
+++ app/src/main/java/com/littlebencreations/roombcontrol/MainActivity.java
@@ -10,32 +10,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.felhr.usbserial.UsbSerialDevice;
 import com.felhr.usbserial.UsbSerialInterface;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
 
+import cz.msebera.android.httpclient.Header;
+
+/**
+ *
+ */
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
     public final String ACTION_USB_PERMISSION = "com.hariharan.arduinousb.USB_PERMISSION";
 
-    //Button startButton, sendButton, clearButton, stopButton;
+    //private Button startButton, sendButton, clearButton, stopButton;
+    private Button connectButton, disconnectButton, forwardButton, backwardButton;
+    private TextView distanceText;
+    private UsbManager usbManager;
+    private UsbDevice device;
+    private UsbSerialDevice serialPort;
+    private UsbDeviceConnection connection;
 
-    Button connectButton, disconnectButton, forwardButton, backwardButton,
-    TextView distanceText;
-    UsbManager usbManager;
-    UsbDevice device;
-    UsbSerialDevice serialPort;
-    UsbDeviceConnection connection;
+    private PopServer mServer;
+    private RoombaController mRoombaRoombaController;
 
-    UsbSerialInterface.UsbReadCallback mCallback = new UsbSerialInterface.UsbReadCallback() { //Defining a Callback which triggers whenever data is read.
+    private UsbSerialInterface.UsbReadCallback mCallback = new UsbSerialInterface.UsbReadCallback() { //Defining a Callback which triggers whenever data is read.
         @Override
-        public void onReceivedData(byte[] arg0) {
+        public void onReceivedData(byte[] bytes) {
             String data = null;
             try {
-                data = new String(arg0, "UTF-8");
+                data = new String(bytes, "UTF-8");
                 data.concat("/n");
                 //tvAppend(textView, data);
             } catch (UnsupportedEncodingException e) {
@@ -45,11 +56,7 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-
-    void setUiEnabled(boolean state) {
-
-    }
-
+    /* This should be an intent filter in the manifest methinks? */
     private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() { //Broadcast Receiver to automatically start and stop the Serial connection.
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -67,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
                             serialPort.setParity(UsbSerialInterface.PARITY_NONE);
                             serialPort.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF);
                             serialPort.read(mCallback);
-                            tvAppend(textView,"Serial Connection Opened!\n");
+                            //tvAppend(textView,"Serial Connection Opened!\n");
 
                         } else {
                             Log.d("SERIAL", "PORT NOT OPEN");
@@ -79,20 +86,39 @@ public class MainActivity extends AppCompatActivity {
                     Log.d("SERIAL", "PERM NOT GRANTED");
                 }
             } else if (intent.getAction().equals(UsbManager.ACTION_USB_DEVICE_ATTACHED)) {
-                onClickStart(startButton);
+                //onClickStart(startButton);
             } else if (intent.getAction().equals(UsbManager.ACTION_USB_DEVICE_DETACHED)) {
-                onClickStop(stopButton);
-
+                //onClickStop(stopButton);
             }
         }
-
-        ;
     };
 
-
-            @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mServer = new PopServer(this,  new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                super.onSuccess(statusCode, headers, response);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+                Toast.makeText(getApplicationContext(), "Error with server: " + errorResponse.toString(),
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+        );
     }
+
+    /**
+     * What does this do?
+     * @param state
+     */
+    void setUiEnabled(boolean state) {
+
+    }
+
 }
---
+++ app/src/main/java/com/littlebencreations/roombcontrol/RoombaController.java
@@ -0,0 +1,14 @@
+package com.littlebencreations.roombcontrol;
+
+/**
+ * Created by austin on 11/22/15.
+ *
+ * Meant to be a simple controller for the Roomba
+ */
+public class RoombaController {
+    private static final String LOG_TAG = RoombaController.class.getSimpleName();
+
+    public RoombaController() {
+
+    }
+}
